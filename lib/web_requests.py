d177984fb284d2b98f9c00e5e3412e3f13ea1477
Marco Bazzani <marco@bazzani.net>
2025-12-30 22:38:53 +0100

    Support for joint account (works only with cURL not with HAR file)

--- lib/web_requests.py
+++ lib/web_requests.py
@@ -9,24 +9,43 @@ logger = logging.getLogger('revol_ver')
 class WebRequests:
 
     @classmethod
-    def fetch_trans(cls, cookie: str, device_id: str, pocket_id:str, to_param: int = 0) -> list[dict]:
+    def fetch_trans(cls, cookie: str, device_id: str, pocket_id:str = '', wallet_id: str = '', account_type: str = '', to_param: int = 0) -> list[dict]:
         '''
         Simulates a request made by Revolut web app
         '''
         url = "https://app.revolut.com/api/retail/user/current/transactions/last"
+        
         params = {
             "count": 500,
-            "internalPocketId": pocket_id
         }
+
+        referer_account_param = ''
+        if wallet_id:
+            params["walletId"] = wallet_id
+            referer_account_param = f'walletId={wallet_id}'
+            if pocket_id: # If both walletId and pocketId are present, add pocketId to referer
+                referer_account_param += f'&pocketId={pocket_id}'
+        elif pocket_id:
+            params["internalPocketId"] = pocket_id
+            referer_account_param = f'accountId={pocket_id}'
+
+
         if to_param != 0:
             params['to'] = to_param
+
+        referer_base = 'https://app.revolut.com/home'
+        if account_type:
+            referer_base += f'?accountType={account_type}&{referer_account_param}'
+        elif referer_account_param:
+            referer_base += f'?{referer_account_param}'
+
 
         headers = {
             'accept': 'application/json, text/plain, */*',
             'accept-language': 'en-US,en;q=0.9,sl;q=0.8',
             'cookie': cookie,
             'priority': 'u=1, i',
-            'referer': f'https://app.revolut.com/transactions?accountId={pocket_id}',
+            'referer': referer_base,
             'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Microsoft Edge";v="126"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
@@ -49,12 +68,12 @@ class WebRequests:
         return response
 
     @classmethod
-    def get_monthly_transactions(cls, cookie: str, device_id: str, pocket_id:str, epoch: int) -> list[dict]:
+    def get_monthly_transactions(cls, cookie: str, device_id: str, epoch: int, pocket_id:str = '', wallet_id: str = '', account_type: str = '') -> list[dict]:
         logger.info('Fetching monthly transactions')
-        return cls.fetch_trans(cookie=cookie, device_id=device_id, pocket_id=pocket_id, to_param=epoch)
+        return cls.fetch_trans(cookie=cookie, device_id=device_id, pocket_id=pocket_id, wallet_id=wallet_id, account_type=account_type, to_param=epoch)
 
     @classmethod
-    def get_all_transactions(cls, cookie: str, device_id: str, pocket_id:str) -> list[dict]:
+    def get_all_transactions(cls, cookie: str, device_id: str, pocket_id:str = '', wallet_id: str = '', account_type: str = '') -> list[dict]:
         '''
         Revolut will take epoch and return certain number of results BEFORE this date.
         This function generates epochs for each month and fetches transactions
@@ -62,7 +81,7 @@ class WebRequests:
         transactions = []
         logger.info('Fetching all transactions')
         for epoch in cls.generate_dates():
-            transactions += cls.fetch_trans(cookie=cookie, device_id=device_id, pocket_id=pocket_id, to_param=epoch)
+            transactions += cls.fetch_trans(cookie=cookie, device_id=device_id, pocket_id=pocket_id, wallet_id=wallet_id, account_type=account_type, to_param=epoch)
         return transactions
 
     @classmethod
